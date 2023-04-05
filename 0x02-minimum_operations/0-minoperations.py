648bc119222e974fe4b254897b5bb5670feeec64
Benterf123 <105057200+Benterf123@users.noreply.github.com>
2023-04-05 23:34:16 +0300

    correction

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
 
-"""
-    Method to determine the quantity of minmum operations in n characters
-"""
-
+"""Minimum operations"""
 
 def minOperations(n):
-   """
-    Calculate the fewest number of operations needed to get 'n' H characters in a file
-    """
-
-    now = 1
-    start = 0
+    
+    """Calculates the minimum number of operations needed to produce n H characters in the file."""
+    
+    if n <= 1:
+        return 0
+    
     count = 0
-    while now < n:
-        rem = n - now
-        if (rem % now == 0):
-            start = now
-            now = now + start
-            count = count + 2
+    i = 2
+    
+    while i <= n:
+        if n % i == 0:
+            count += i
+            n //= i
         else:
-            now = now + start
-            count = count + 1
+            i += 1
+            
     return count
-
-# !/usr/bin/python3
-
-#     """
-#     Calculate the fewest number of operations needed to get 'n' H characters in a file
-#     """
-#     def minOperations(n):
-#     count = 0
-#     while n > 1:
-#         if n % 2 == 0:
-#             n //= 2
-#         else:
-#             n -= 1
-#         count += 1
-#     return count
