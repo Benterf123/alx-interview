0a7b9c2614dc6decad64edafb2b8c3279f32059f
Benterf123 <105057200+Benterf123@users.noreply.github.com>
2023-04-05 23:43:51 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -12,7 +12,7 @@ def minOperations(n):
     count = 0
     i = 2
     
-    while i > n:
+    while (i <= n):
         if n % i == 0:
             count += i
             n //= i
@@ -20,26 +20,3 @@ def minOperations(n):
             i += 1
             
     return count
-#####
-#
-
-# f n < 1:
-#         return 0
-    
-#     count = 0
-#     i = 2
-    
-#     while n > 1:
-#         if n % i == 0:
-#             count += i
-#             n //= i
-#         else:
-#             div += 1
-            
-#     return count
-# This code correctly calculates the minimum number of operations needed to produce n H characters in the file.
-
-
-
-
-
